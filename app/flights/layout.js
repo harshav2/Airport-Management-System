eed622eb69afdaec3a7e16f60149b8bc531cfaed
Harsha V <harsha.vishwanath2004@gmail.com>
2024-10-23 12:15:20 +0530

    initial testing

--- app/flights/layout.js
+++ app/flights/layout.js
@@ -1,13 +1,13 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import "../globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -19,14 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <p>below the body </p>
-        {children}
-        <p>Hello from the other sode</p>
-      </body>
-    </html>
+    // <html lang="en">
+    <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <p>layout</p>
+      {children}
+      <p>layout</p>
+    </div>
+    // </html>
   );
 }
---
+++ app/flights/page.js
@@ -0,0 +1,4 @@
+const flights = () => {
+  return <p>ello</p>;
+};
+export default flights;
